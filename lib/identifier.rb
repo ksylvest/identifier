beef102685357e1115b32f8c1313da0b26c630b7
Kevin Sylvestre <kevin@ksylvest.com>
2011-06-25 13:48:21 -0400

    fixed identifier to use the generate option

--- lib/identifier.rb
+++ lib/identifier.rb
@@ -1,25 +1,4 @@
+require 'identifier/identifier'
+
 module Identifier
-  
-  # Generate a universally unique identifier on a UNIX, Linx or Mac OS X system.
-  #
-  # Usage:
-  #
-  #   Identifier.generate # 53FB3F0B-C19A-41E1-8718-5BABE85CD341
-  
-  def self.generate
-    begin
-      result = `uuidgen`
-    rescue Errno::ENOENT
-      result = `/bin/uuidgen`
-    rescue Errno::ENOENT
-      result = `/sbin/uuidgen`
-    rescue Errno::ENOENT
-      result = `/usr/bin/uuidgen`
-    rescue Errno::ENOENT
-      result = `/usr/sbin/uuidgen`
-    end
-    
-    result.strip
-  end
-  
 end
