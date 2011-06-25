c207b80abe8e91d9380609037fb4e43f22eeca4d
Kevin Sylvestre <kevin@ksylvest.com>
2011-06-25 16:02:32 -0400

    added generate code within ruby

--- lib/identifier.rb
+++ lib/identifier.rb
@@ -1,4 +1,27 @@
 require 'identifier/identifier'
 
+begin
+require 'securerandom' 
+rescue LoadError
+
 module Identifier
+  
+  def generate
+    SecureRandom.uuid if defined? SecureRandom
+    
+    bytes = [
+      rand(0xFFFFFFFF),
+      rand(0x0000FFFF),
+      rand(0x0000FFFF),
+      rand(0x0000FFFF),
+      rand(0x0000FFFF),
+      rand(0xFFFFFFFF),
+    ]
+    
+    bytes[2] = (bytes[2] & 0x0FFF) | 0x4000
+    bytes[3] = (bytes[3] & 0x3FFF) | 0x8000
+    
+    "%08x-%04x-%04x-%04x-%04x%08x" % bytes
+  end
+  
 end
