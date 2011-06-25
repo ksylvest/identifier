3b653de654d7ad06af219a2edcdd6bfbed08ec91
Kevin Sylvestre <kevin@ksylvest.com>
2011-06-25 13:47:04 -0400

    added test to project

---
+++ test/test.rb
@@ -0,0 +1,20 @@
+require 'identifier'
+
+require 'test/unit'
+
+class TestIdentifier < Test::Unit::TestCase
+  
+  FORMAT = /\A\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\Z/
+
+  def test_format
+    identifier = Identifier.generate
+    assert(identifier =~ FORMAT, "identifiers should match a consistent format")
+  end
+  
+  def test_unique
+    alpha = Identifier.generate
+    omega = Identifier.generate
+    assert_not_equal(alpha, omega, "identifiers should be sequentially unique")
+  end
+  
+end
