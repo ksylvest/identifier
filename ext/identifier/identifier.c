77403687d4b26986b86a8e40948e62925dc403a8
Kevin Sylvestre <kevin@ksylvest.com>
2011-06-27 11:57:45 -0400

    added use of compiler constants

--- ext/identifier/identifier.c
+++ ext/identifier/identifier.c
@@ -1,10 +1,15 @@
 #include <ruby.h>
+
+#ifdef UUID
 #include <uuid/uuid.h>
+#endif
 
 VALUE Identifier;
 
 VALUE method_generate(VALUE self)
 {
+#ifdef UUID
+  
   char result[] = "00000000-0000-0000-0000-000000000000";
   
   uuid_t uuid;
@@ -12,10 +17,16 @@ VALUE method_generate(VALUE self)
   uuid_unparse(uuid, result);
   
   return rb_str_new(result, strlen(result));
+  
+#endif
 }
 
 void Init_identifier() 
 {
+#ifdef UUID
+  
   Identifier = rb_define_module("Identifier");
   rb_define_singleton_method(Identifier, "generate", method_generate, 0);
+  
+#endif
 }
